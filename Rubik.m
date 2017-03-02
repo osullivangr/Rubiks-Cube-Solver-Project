01bbae7d50f01918ddb8e7b89e6ad7011df43327
michaelmoynihan <michaeljmoynihan97@gmail.com>
2017-03-02 09:20:13 -0500

    Add files via upload

--- Rubik.m
+++ Rubik.m
@@ -6,23 +6,22 @@
 
 classdef Rubik
     properties
-        cubes = [cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3));
-            cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3));
-            cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3))];
-        colors = [1,1,1;0,1,0;1,0,0;1,1,0;0,0,1;1,0.7,0];
+        cubes = [cube(0,0,0,1),cube(0,0,0,1),cube(0,0,0,1);
+                cube(0,0,0,1),cube(0,0,0,1),cube(0,0,0,1);
+                cube(0,0,0,1),cube(0,0,0,1),cube(0,0,0,1)];
     end
     methods
         function obj = Rubik()
-            obj.cubes(:,:,2) = [cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3));
-                cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3));
-                cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3))];
-            obj.cubes(:,:,3) = [cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3));
-                cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3));
-                cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3)),cube(0,0,0,1,zeros(6,3))];
+            obj.cubes(:,:,2) = [cube(0,0,0,1),cube(0,0,0,1),cube(0,0,0,1);
+                cube(0,0,0,1),cube(0,0,0,1),cube(0,0,0,1);
+                cube(0,0,0,1),cube(0,0,0,1),cube(0,0,0,1)];
+            obj.cubes(:,:,3) = [cube(0,0,0,1),cube(0,0,0,1),cube(0,0,0,1);
+                cube(0,0,0,1),cube(0,0,0,1),cube(0,0,0,1);
+                cube(0,0,0,1),cube(0,0,0,1),cube(0,0,0,1)];
             for i=1:3
                 for j=1:3
                     for k=1:3
-                        obj.cubes(k, j, i) = cube(i-2.5, j-2.5, k-2.5 , 1, obj.colors);
+                        obj.cubes(k, j, i) = cube(i-2.5, j-2.5, k-2.5 , 1);
                     end
                 end
             end
@@ -39,18 +38,16 @@ classdef Rubik
             axis square;
             set(gca,'visible','off');
         end
-        function rotateFace(centerColor, clockWise)
-            %{
-            switch(centerColor)
-                case 1
-                    if (clockWise)
-                        newLayer = [cube(0,0,0,1,zeros(6,3))];
-                        k = 3;
-                        for i=1:3
-                            for j=1:3
-                                newLayer(j, i) = 
-                            end
-            %}
-        end
+%        function rotateFace(centerColor, clockWise)
+%             switch(centerColor)
+%                 case 1
+%                     if (clockWise)
+%                         newLayer = [cube(0,0,0,1,zeros(6,3))];
+%                         k = 3;
+%                         for i=1:3
+%                             for j=1:3
+%                                 newLayer(j, i) = 
+%                             end
+%        end
     end
 end
--- cube.m
+++ cube.m
@@ -15,12 +15,12 @@ classdef cube
         faces
     end
     methods
-        function obj = cube(X, Y, Z, s, colors)
+        function obj = cube(X, Y, Z, s)
             obj.X = X;
             obj.Y = Y;
             obj.Z = Z;
             obj.s = s;
-            obj.colors = colors;
+            obj.colors = [1,1,1;0,1,0;1,0,0;1,1,0;0,0,1;1,0.7,0];
             obj.vertices = [
                 obj.X, obj.Y, obj.Z+obj.s;
                 obj.X+obj.s, obj.Y, obj.Z+obj.s;
