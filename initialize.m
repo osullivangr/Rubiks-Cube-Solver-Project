a338059cb959176f2512cdb744dd17f9008f8068
michaelmoynihan <michaeljmoynihan97@gmail.com>
2017-03-30 15:54:17 -0400

    Update and rename cube.m to initialize.m

---
+++ initialize.m
@@ -0,0 +1,33 @@
+% initialize.m
+
+%% Set Face Colors
+faces = [
+    Color([1,1,1]),Color([1,1,1]),Color([1,1,1]);
+    Color([1,1,1]),Color([1,1,1]),Color([1,1,1]);
+    Color([1,1,1]),Color([1,1,1]),Color([1,1,1])
+];
+faces(:,:,2) = [
+    Color([1,0,0]),Color([1,0,0]),Color([1,0,0]);
+    Color([1,0,0]),Color([1,0,0]),Color([1,0,0]);
+    Color([1,0,0]),Color([1,0,0]),Color([1,0,0])
+];
+faces(:,:,3) = [
+    Color([0,1,0]),Color([0,1,0]),Color([0,1,0]);
+    Color([0,1,0]),Color([0,1,0]),Color([0,1,0]);
+    Color([0,1,0]),Color([0,1,0]),Color([0,1,0])
+];
+faces(:,:,4) = [
+    Color([1,1,0]),Color([1,1,0]),Color([1,1,0]);
+    Color([1,1,0]),Color([1,1,0]),Color([1,1,0]);
+    Color([1,1,0]),Color([1,1,0]),Color([1,1,0])
+];
+faces(:,:,5) = [
+    Color([1,0.7,0]),Color([1,0.7,0]),Color([1,0.7,0]);
+    Color([1,0.7,0]),Color([1,0.7,0]),Color([1,0.7,0]);
+    Color([1,0.7,0]),Color([1,0.7,0]),Color([1,0.7,0])
+];
+faces(:,:,6) = [
+    Color([0,0,1]),Color([0,0,1]),Color([0,0,1]);
+    Color([0,0,1]),Color([0,0,1]),Color([0,0,1]);
+    Color([0,0,1]),Color([0,0,1]),Color([0,0,1])
+];
