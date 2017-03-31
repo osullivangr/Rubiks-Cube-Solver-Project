7f8e365415ec684bb16974ad728ee54bf2c357c4
michaelmoynihan <mmoyniha@nd.edu>
2017-03-31 10:49:12 -0400

    Update turnFront.m

--- turnFront.m
+++ turnFront.m
@@ -1,25 +1,38 @@
 % turnFront.m
 
 function faces = turnFront(faces, clockwise)
+
+    % Clockwise
     if (clockwise)
+    
+        % Rotate face
         faces(:,:,1) = faces(:,:,1)';
         temp = faces(:,1,1);
         faces(:,1,1) = faces(:,3,1);
         faces(:,3,1) = temp;
+        
+        % Rotate edges
         temp = faces(3,:,2);
         faces(3,:,2) = flip(faces(:,3,6));
         faces(:,3,6) = faces(:,3,5);
         faces(:,3,5) = flip(faces(3,:,3));
         faces(3,:,3) = temp;
+        
+    % Counterclockwise
     else
+    
+        % Rotate face
         temp = faces(:,1,1);
         faces(:,1,1) = faces(:,3,1);
         faces(:,3,1) = temp;
         faces(:,:,1) = faces(:,:,1)';
+        
+        % Rotate edges
         temp = faces(3,:,2);
         faces(3,:,2) = faces(3,:,3);
         faces(3,:,3) = flip(faces(:,3,5));
         faces(:,3,5) = faces(:,3,6);
         faces(:,3,6) = flip(temp);
+        
     end
 end
