92641297990859dd2ac9038a2afb6433f62701f2
michaelmoynihan <mmoyniha@nd.edu>
2017-03-31 10:52:01 -0400

    Update turnTop.m

--- turnTop.m
+++ turnTop.m
@@ -1,20 +1,33 @@
 % turnTop.m
+
 function faces = turnTop(faces, clockwise)
+
+    % Clockwise
     if (clockwise)
+    
+        % Rotate face
         faces(:,:,2) = faces(:,:,2)';
         temp = faces(:,1,2);
         faces(:,1,2) = faces(:,3,2);
         faces(:,3,2) = temp;
+        
+        % Rotate edges
         temp = faces(1,:,1);
         faces(1,:,1) = flip(faces(:,1,3));
         faces(:,1,3) = faces(:,1,4);
         faces(:,1,4) = flip(faces(1,:,6));
         faces(1,:,6) = temp;
+        
+    % Counterclockwise
     else
+    
+        % Rotate face
         temp = faces(:,1,2);
         faces(:,1,2) = faces(:,3,2);
         faces(:,3,2) = temp;
         faces(:,:,2) = faces(:,:,2)';
+        
+        % Rotate edges
         temp = faces(1,:,1);
         faces(1,:,1) = faces(1,:,6);
         faces(1,:,6) = flip(faces(:,1,4));
