245589601229e8d62d53ab08ce8045c49a0f7b59
osullivangr <osullivangr@gmail.com>
2017-04-17 19:26:49 -0400

    Create Jb_perm.m

---
+++ Jb_perm.m
@@ -0,0 +1,23 @@
+function faces=Jb_perm(faces)
+% preforms the Y algorithm
+% U' [R' U L'] [U2 R U' R' U2] [R L]
+
+clockwise=1;
+CCW=0;
+
+faces=turnTop(faces,CCW);
+
+faces=turnRight(faces,CCW);
+faces=turnTop(faces,clockwise);
+faces=turnLeft(faces,CCW);
+
+faces=turnTop(faces,clockwise);
+faces=turnTop(faces,clockwise);
+faces=turnRight(faces,clockwise);
+faces=turnTop(faces,CCW);
+faces=turnRight(faces,CCW);
+faces=turnTop(faces,CCW);
+faces=turnTop(faces,CCW);
+
+faces=turnRight(faces,clockwise);
+faces=turnLeft(faces,clockwise);
