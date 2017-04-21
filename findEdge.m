5bb41ff79fb365dbdfc174cdbdb5faa4355312bf
michaelmoynihan <mmoyniha@nd.edu>
2017-04-21 15:23:41 -0400

    Update findEdge.m

--- findEdge.m
+++ findEdge.m
@@ -111,14 +111,8 @@ function [i1,j1,k1,i2,j2,k2,c1,c2] = findEdge(faces, c1, c2)
         end
 
         % find if the piece in top and bottom is in the correct position
-        if (top == c1 && bottom == c2)
-            c1 = top;
-            c2 = bottom;
-            break;
-        elseif (top == c2 && bottom == c1)
-            c1 = bottom;
-            c2 = top;
-            break;
+        if (top == c1 && bottom == c2) || (top == c2 && bottom == c1)
+            return;
         end
     end
 end
