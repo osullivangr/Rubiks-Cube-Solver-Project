73422e15317e8b5ae9a0794c740a7f512c22cdba
michaelmoynihan <mmoyniha@nd.edu>
2017-04-24 10:50:11 -0400

    Update doWhiteCorners.m

--- doWhiteCorners.m
+++ doWhiteCorners.m
@@ -29,7 +29,7 @@ function faces = doWhiteCorners(faces)
             faces = turnBottom(faces, 1);
             [~,~,k1,~,~,k2,~,~,k3] = findCorner(faces,[1,1,1],colors(i,:),colors(mod(i,4)+1,:));
         end
-        while (k1 ~= 2 && k2 ~= 2 && k3 ~= 2) || ~arrayEqual(faces(3,3,2).getColor,[1,1,1])
+        while ~arrayEqual(faces(1,3,1).getColor,colors(i,:)) || ~arrayEqual(faces(3,3,2).getColor,[1,1,1]) || ~arrayEqual(faces(3,1,3).getColor,colors(mod(i,4)+1,:))
             faces = turnRight(faces, 0);
             faces = turnBottom(faces, 0);
             faces = turnRight(faces, 1);
